bdb6779ca231cfcc70c57fa149be103357b9bba9
lucapxl <sariluca@gmail.com>
2025-09-03 09:47:36 -0400

    added new gestures and corrected fuzzel config

--- save_configs.sh
+++ save_configs.sh
@@ -3,6 +3,8 @@
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 FILES_DIR=$SCRIPTPATH/files/
+FILES_DIR_CONFIG=$FILES_DIR/config
+FILES_DIR_THEMES=$FILES_DIR/themes
 
 echo $SCRIPTPATH
 echo $FILES_DIR
@@ -10,16 +12,19 @@ echo $FILES_DIR
 rm -rf $SCRIPTPATH/files/*
 
 mkdir -p $FILES_DIR
-cp -R ~/.config/bg.jpg $FILES_DIR
-cp -R ~/.config/foot $FILES_DIR
-cp -R ~/.config/dunst $FILES_DIR
-cp -R ~/.config/gammastep $FILES_DIR
-cp -R ~/.config/scripts $FILES_DIR
-cp -R ~/.config/swaylock $FILES_DIR
-cp -R ~/.config/wlogout $FILES_DIR
-cp -R ~/.config/kanshi $FILES_DIR
-cp -R ~/.config/sfwbar $FILES_DIR
-cp -R ~/.config/labwc $FILES_DIR
-cp -R ~/.config/fuzzel $FILES_DIR
-cp -R ~/.config/xdg-desktop-portal $FILES_DIR
-cp -R ~/.themes $FILES_DIR
+mkdir -p $FILES_DIR_CONFIG
+mkdir -p $FILES_DIR_THEMES
+
+cp -R ~/.config/bg.jpg $FILES_DIR_CONFIG
+cp -R ~/.config/foot $FILES_DIR_CONFIG
+cp -R ~/.config/dunst $FILES_DIR_CONFIG
+cp -R ~/.config/gammastep $FILES_DIR_CONFIG
+cp -R ~/.config/scripts $FILES_DIR_CONFIG
+cp -R ~/.config/swaylock $FILES_DIR_CONFIG
+cp -R ~/.config/wlogout $FILES_DIR_CONFIG
+cp -R ~/.config/kanshi $FILES_DIR_CONFIG
+cp -R ~/.config/sfwbar $FILES_DIR_CONFIG
+cp -R ~/.config/labwc $FILES_DIR_CONFIG
+cp -R ~/.config/fuzzel $FILES_DIR_CONFIG
+cp -R ~/.config/xdg-desktop-portal $FILES_DIR_CONFIG
+cp -R ~/.themes/* $FILES_DIR_THEMES
