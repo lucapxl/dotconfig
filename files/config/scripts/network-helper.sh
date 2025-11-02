c99da4fbf8852e73f500f76ff68e70e7827fa9ca
Luca <sariluca@gmail.com>
2025-11-03 00:01:22 +0100

    back to waybar for real

--- files/config/scripts/network-helper.sh
+++ files/config/scripts/network-helper.sh
@@ -14,6 +14,15 @@ case "$1" in
         down)
                 nmcli connection down "$2"
                 ;;
+        toggle)
+                status=$(nmcli -t dev status |grep "$2" | wc -l)
+                echo "$status"
+                if [ $status -eq 1 ]; then
+                        nmcli connection down "$2"
+                else
+                        nmcli connection up "$2"
+                fi
+                ;;
         *)
                 echo "use 'connections' as argument"
                 ;;
