2612c1ff02af6e9a4e5ddd911fed1c12c341b83d
Luca <sariluca@gmail.com>
2025-11-06 23:09:14 +0100

    bugfixes and stilistic change in waybar

---
+++ files/config/scripts/labwc-keyboardlang-helper.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+case "$1" in
+        status)
+                IFS=','
+                read -a keyboard_values <<< "$XKB_DEFAULT_LAYOUT"
+                brightnessFileName=$(ls /sys/class/leds/input*\:\:scrolllock/brightness | head -n 1)
+                echo "${keyboard_values[$(cat $brightnessFileName)]}"
+                ;;
+        *)
+                echo "use 'status' as argument"
+                ;;
+esac
--- files/config/scripts/sfwbar-keyboardlang-helper.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-case "$1" in
-        status)
-                if [ "$(cat /sys/class/leds/input*\:\:scrolllock/brightness | head -n 1)" == "1" ]; then echo 'US';else echo 'CH'; fi
-                ;;
-        *)
-                echo "use 'status' as argument"
-                ;;
-esac
